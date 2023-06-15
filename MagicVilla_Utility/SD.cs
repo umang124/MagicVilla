f57307c2ae4178f2f811a1f06e7a5a2b22d90baf
Umang Shrestha <umangstha124@gmail.com>
2023-06-15 19:09:16 +0545

    add APIRequest class and DTOs in web project

---
+++ MagicVilla_Utility/SD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicVilla_Utility
+{
+    public enum ApiType
+    {
+        GET,
+        PUT,
+        POST,
+        DELETE
+    }
+}
---
+++ MagicVilla_Web/APIRequest.cs
@@ -0,0 +1,11 @@
+﻿using MagicVilla_Utility;
+
+namespace MagicVilla_Web
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
