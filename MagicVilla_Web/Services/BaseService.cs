8f886bc14dbfe14276f05828395e9fbf2f04e7cf
Umang Shrestha <umangstha124@gmail.com>
2023-06-15 22:42:48 +0545

    base service for api

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,75 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_VillaAPI;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MagicVilla_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            responseModel = new APIResponse();
+            this.httpClient = httpClient;
+        }
+
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MagicAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                                        Encoding.UTF8, "application/json");
+                }
+
+                switch (apiRequest.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case ApiType.GET:
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                HttpResponseMessage apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                if (APIResponse != null)
+                    return APIResponse;
+
+            }
+            catch (Exception ex)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                if (APIResponse != null)
+                    return APIResponse;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MagicVilla_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_VillaAPI;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
