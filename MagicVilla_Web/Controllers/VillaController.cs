26d0b6498bf89a8ad543f243c2625706ecff5fda
Umang Shrestha <umangstha124@gmail.com>
2023-06-22 13:15:08 +0545

    dissabled nullable and finished UI design for create villa

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using MagicVilla_VillaAPI;
+using MagicVilla_VillaAPI.DTOs;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,39 @@ namespace MagicVilla_Web.Controllers
                 list = JsonConvert.DeserializeObject<List<Villa>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateVilla()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _villaService.CreateAsync<APIResponse>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteVilla(int id)
+        {
+            return View();
         }
     }
 }
